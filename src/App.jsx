20d1924d4a5372cd1725ee10dde54f846f61aaf3
sreey <sreelakshmi12898@gmail.com>
2023-06-01 12:45:07 +0530

    Take user input and display it

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
+import React, { useState } from 'react'
 import styles from './App.module.css';
 import UserForm from './components/form/UserForm';
 import UserList from './components/list/UserList';
 
 function App() {
 
-  const usersList = [
+  const [usersList, setUserList] = useState([
     {name:' Mark', age: 31, id: 'u1'},
     {name: 'Sara', age: 25, id: 'u2'}
-  ];
+  ]);
+
+  const getUser = (newUser) => {
+    setUserList(prevList => {
+      return [newUser, ...prevList]
+    })
+  }
   
   return (
     <div>
       <section className={styles.form} >
-        <UserForm />
+        <UserForm onInput={getUser} />
       </section>
       <section className={styles.user} >
         <UserList users={usersList} />
--- src/components/form/UserForm.jsx
+++ src/components/form/UserForm.jsx
@@ -1,14 +1,39 @@
+import React, { useState } from 'react';
 import styles from './UserForm.module.css';
 import Button from './Button';
 
-function UserForm(){
+function UserForm(props){
+
+    const [username, setUsername] = useState('');
+    const [userAge, setUserAge] = useState('');
+
+    const getUsername = (event) => {
+        setUsername(event.target.value);
+    }
+
+    const getUserAge = (event) => {
+        setUserAge(event.target.value);
+    }
+
+    const getUserData = (event) => {
+        event.preventDefault();
+
+        const userData = {
+            name: username,
+            age: +userAge,
+            id: Math.random().toString()
+        };
+        props.onInput(userData);
+        setUsername('');
+        setUserAge('');
+    }
 
     return(
-        <form className={styles["user-form"]}>
+        <form onSubmit={getUserData} className={styles["user-form"]}>
             <label>Username:</label>
-            <input type="text" />
+            <input value={username} type="text" onChange={getUsername} />
             <label>Age(Years):</label>
-            <input type="number" min={13} />
+            <input value={userAge} type="number" min={13} onChange={getUserAge} />
             <Button type='submit' >Add User</Button>
         </form>
     );
